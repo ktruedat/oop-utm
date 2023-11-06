c1adbf4a886b989feee77c90436bca0456e0db0c
ktruedat <100484364+ktruedat@users.noreply.github.com>
2023-11-06 12:04:41 +0200

    Update sarcina2.cpp

--- Lab2/sarcina2.cpp
+++ Lab2/sarcina2.cpp
@@ -3,95 +3,238 @@
 
 class Matrix {
 private:
-    long* data; // Pointer către matrice
-    int numRows; // Numărul de rânduri
-    int numCols; // Numărul de coloane
-    int errorCode; // Codul de eroare (0 = fără eroare, 1 = eroare de dimensiune, 2 = eroare de memorie)
+    long* data;
+    int rows;
+    int cols;
+    int errorCode;
 
 public:
-    // Constructor implicit (fără parametri)
-    Matrix() : data(nullptr), numRows(0), numCols(0), errorCode(0) {}
+    Matrix() : data(nullptr), rows(0), cols(0), errorCode(0) {}
 
-    // Constructor pentru o matrice pătrată de dimensiune n x n
-    Matrix(int n) : numRows(n), numCols(n), errorCode(0) {
+    Matrix(int n) : rows(n), cols(n), errorCode(0) {
         if (n <= 0) {
-            errorCode = 1; // Eroare de dimensiune
+            errorCode = 1; 
             return;
         }
-        data = new long[n * n];
+        data = new long[n * n]();
     }
 
-    // Constructor pentru o matrice dreptunghiulară de dimensiune numRows x numCols
-    Matrix(int numRows, int numCols) : numRows(numRows), numCols(numCols), errorCode(0) {
-        if (numRows <= 0 || numCols <= 0) {
-            errorCode = 1; // Eroare de dimensiune
+    Matrix(int x, int y) : rows(x), cols(y), errorCode(0) {
+        if (x <= 0 || y <= 0) {
+            errorCode = 1;
             return;
         }
-        data = new long[numRows * numCols];
+        data = new long[x * y]();
     }
 
-    // Funcție pentru a obține valoarea elementului (i, j)
-    long getElement(int i, int j) {
-        if (i < 0 || i >= numRows || j < 0 || j >= numCols) {
-            errorCode = 1; // Eroare de dimensiune
-            throw std::out_of_range("Indici incorecți");
-        }
-        return data[i * numCols + j];
-    }
-
-    // Funcție pentru a seta valoarea elementului (i, j)
-    void setElement(int i, int j, long value) {
-        if (i < 0 || i >= numRows || j < 0 || j >= numCols) {
-            errorCode = 1; // Eroare de dimensiune
-            throw std::out_of_range("Indici incorecți");
-        }
-        data[i * numCols + j] = value;
-    }
-
-    // Funcție pentru a verifica dacă există erori
-    bool hasError() {
-        return errorCode != 0;
-    }
-
-    // Funcție pentru a afișa matricea
-    void printMatrix() {
-        if (hasError()) {
-            std::cout << "Eroare: ";
-            if (errorCode == 1) {
-                std::cout << "Eroare de dimensiune";
-            } else if (errorCode == 2) {
-                std::cout << "Eroare de memorie";
-            }
-            std::cout << std::endl;
-            return;
-        }
-
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                std::cout << data[i * numCols + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    // Destructor pentru eliberarea memoriei
     ~Matrix() {
-        delete[] data;
+         delete[] data; 
+        //  Matricea data a fost alocată ca un singur bloc de memorie, nu ca un vector de pointeri la alte matrice.
     }
+
+    void setElement(int row, int col, long value) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
+            errorCode = 2;
+            return;
+        }
+        data[row * cols + col] = value;
+    }
+
+    long getElement(int row, int col) const {
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
+            // errorCode = 2;
+            return 0;
+        }
+        return data[row * cols + col];
+    }
+
+    int getCols(){
+        return cols;
+    }
+
+     int getRows(){
+        return rows;
+    }
+
+    Matrix add(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols) {
+            // errorCode = 3;
+            return Matrix();
+        }
+
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.setElement(i, j, getElement(i, j) + other.getElement(i, j));
+            }
+        }
+        return result;
+    }
+
+    Matrix subtract(const Matrix& other) const {
+        if (rows != other.rows || cols != other.cols) {
+            // errorCode = 3;
+            return Matrix();
+        }
+
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.setElement(i, j, getElement(i, j) - other.getElement(i, j));
+            }
+        }
+        return result;
+    }
+
+    Matrix multiply(const Matrix& other) const {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.setElement(i, j, getElement(i, j) * other.getElement(i, j));
+            }
+        }
+
+        return result;
+    }
+
+
+    Matrix multiplyByNumber(long num) const {
+        Matrix result(rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result.setElement(i, j, getElement(i, j) * num);
+            }
+        }
+        return result;
+    }
+
+    int getErrorCode() const {
+        return errorCode;
+    }
+
+    Matrix(const Matrix& other) : rows(other.rows), cols(other.cols), errorCode(other.errorCode) {
+        data = new long[rows * cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                setElement(i, j, other.getElement(i, j));
+            }
+        }
+    }
+
+    Matrix& operator=(const Matrix& other) {
+    if (this != &other) {
+        delete[] data;
+        rows = other.rows;
+        cols = other.cols;
+        errorCode = other.errorCode;
+        data = new long[rows * cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                setElement(i, j, other.getElement(i, j));
+            }
+        }
+    }
+    return *this;
+}
 };
 
 int main() {
-    // Exemplu de utilizare a clasei Matrix
-    Matrix squareMatrix(3); // Matrice pătrată de 3x3
-    squareMatrix.setElement(0, 0, 1);
-    squareMatrix.setElement(1, 1, 2);
-    squareMatrix.setElement(2, 2, 3);
-    squareMatrix.printMatrix();
+    Matrix A(2, 3);
+    A.setElement(0, 0, 1);
+    A.setElement(0, 1, 2);
+    A.setElement(0, 2, 3);
+    A.setElement(1, 0, 4);
+    A.setElement(1, 1, 5);
+    A.setElement(1, 2, 6);
 
-    Matrix rectangularMatrix(2, 4); // Matrice dreptunghiulară de 2x4
-    rectangularMatrix.setElement(0, 0, 4);
-    rectangularMatrix.setElement(1, 2, 7);
-    rectangularMatrix.printMatrix();
+    Matrix B(2, 3);
+    B.setElement(0, 0, 4);
+    B.setElement(0, 1, 5);
+    B.setElement(0, 2, 6);
+    B.setElement(1, 0, 7);
+    B.setElement(1, 1, 8);
+    B.setElement(1, 2, 9);
+
+    std::cout <<"Adunarea\n";
+
+
+    Matrix C = A.add(B);
+
+    if (C.getErrorCode() == 0) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::cout << C.getElement(i, j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    } else {
+        std::cout << "Error: ";
+        if (C.getErrorCode() == 1) {
+            std::cout << "Invalid matrix size or index.";
+        } else if (C.getErrorCode() == 3) {
+            std::cout << "Matrix size mismatch.";
+        }
+    }
+
+    std::cout <<"Diferenta\n";
+
+
+    Matrix D = B.subtract(A);
+    if (D.getErrorCode() == 0) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::cout << D.getElement(i, j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    } else {
+        std::cout << "Error: ";
+        if (D.getErrorCode() == 1) {
+            std::cout << "Invalid matrix size or index.";
+        } else if (D.getErrorCode() == 3) {
+            std::cout << "Matrix size mismatch.";
+        }
+    }
+
+    std::cout <<"Inmultirea cu un nr\n";
+
+
+    Matrix E = A.multiplyByNumber(3);
+     if (E.getErrorCode() == 0) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::cout << E.getElement(i, j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    } else {
+        std::cout << "Error: ";
+        if (E.getErrorCode() == 1) {
+            std::cout << "Invalid matrix size or index.";
+        } else if (E.getErrorCode() == 3) {
+            std::cout << "Matrix size mismatch.";
+        }
+    }
+
+    std::cout <<"Inmultirea a doua matrici\n";
+
+    Matrix F = A.multiply(B);
+
+    if (F.getErrorCode() == 0) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                std::cout << F.getElement(i, j) << " ";
+            }
+            std::cout << std::endl;
+        }
+    } else {
+        std::cout << "Error: ";
+        if (F.getErrorCode() == 1) {
+            std::cout << "Invalid matrix size or index.";
+        } else if (F.getErrorCode() == 3) {
+            std::cout << "Matrix size mismatch.";
+        }
+    }
 
     return 0;
 }
