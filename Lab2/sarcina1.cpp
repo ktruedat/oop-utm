d3283a24857814cb1eca0f7adf61970309dfc985
ktruedat <100484364+ktruedat@users.noreply.github.com>
2023-11-06 12:04:21 +0200

    Update sarcina1.cpp

--- Lab2/sarcina1.cpp
+++ Lab2/sarcina1.cpp
@@ -4,46 +4,44 @@
 
 class File {
 private:
+    std::string filePath;
     std::string filename;
     std::string fileExtension;
     std::size_t fileSize;
     time_t creationTime;
 
 public:
-    File(const std::string& filename, const std::string& extension, std::size_t size) {
-        this->filename = filename;
+    File(const std::string& filePath, const std::string& extension, std::size_t size) {
+        this->filePath = filePath;
+        this->filename = filePath.substr(filePath.find_last_of('/') + 1);
         this->fileExtension = extension;
         this->fileSize = size;
         this->creationTime = std::time(0);
     }
 
-    // Constructor de schimbare a tipului (parametrul - numele de fișier)
     File(const std::string& filename) {
         this->filename = filename;
-        // Presupunem aici că extensia și celelalte atribute sunt necunoscute la momentul creării obiectului.
         this->fileExtension = "";
         this->fileSize = 0;
         this->creationTime = std::time(0);
     }
 
-    // Funcția pentru redenumirea fișierului
     void renameFile(const std::string& newFilename) {
         filename = newFilename;
     }
 
-    // Funcția pentru transfer în alt director
-    void transferToDirectory(const std::string& newDirectory) {
-        // Implementați aici logica de transfer în noul director.
+   void transferToDirectory(const std::string& newDirectory) {
+        std::string newFilePath = newDirectory + filename;
         std::cout << "Transfer " << filename << " to directory " << newDirectory << std::endl;
+        filePath = newFilePath;
     }
 
-    // Funcția pentru modificarea anexei de asociere
     void changeFileExtension(const std::string& newExtension) {
         fileExtension = newExtension;
     }
 
-    // Funcție pentru a afișa informațiile despre fișier
-    void displayFileInfo() {
+     void displayFileInfo() {
+        std::cout << "Drumul fișierului: " << filePath << std::endl;
         std::cout << "Numele fișierului: " << filename << std::endl;
         std::cout << "Extensia: " << fileExtension << std::endl;
         std::cout << "Mărimea: " << fileSize << " bytes" << std::endl;
@@ -52,19 +50,19 @@ public:
 };
 
 int main() {
-    // Exemplu de utilizare a clasei File
     File myFile("document.txt", "doc", 1024);
     myFile.displayFileInfo();
 
-    // Redenumirea fișierului
     myFile.renameFile("new_document.txt");
     myFile.displayFileInfo();
 
-    // Transferul în alt director
+    std::string abc = "pdf";
+    std::string& extension = abc;
+    myFile = extension;
+
     myFile.transferToDirectory("/new/directory/");
     myFile.displayFileInfo();
 
-    // Modificarea anexei de asociere
     myFile.changeFileExtension("pdf");
     myFile.displayFileInfo();
 
